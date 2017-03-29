e2e99512a4c988f8f719925a31fca739580b0d4b
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2017-03-29 17:01:57 +0200

    With Add Books

--- src/RouterComponent.js
+++ src/RouterComponent.js
@@ -1,15 +1,19 @@
 import React from "react";
 import App from "./App";
 import Details from "./Details";
+import { observer } from "mobx-react"
+import bookStore from "./BookStore"
+import NewBook from "./NewBook"
 
-import {hashHistory,Link, IndexRoute,Router, Route} from "react-router"
+import { hashHistory, Link, IndexRoute, Router, Route } from "react-router"
 
-const Home = () => (
+const Home = observer(() => (
   <div>
     <h2>Home View</h2>
+    <p>{bookStore.bookCount}</p>
     <p>Info about this site</p>
   </div>
-)
+))
 
 const Company = () => {
   return (
@@ -23,20 +27,33 @@ const Blog = () => <h2>Our Company Blog</h2>
 
 
 
-const Product = (props) => (
-  <div>
-    <h2>Our Products</h2>
-    <h4>All our great books </h4>
-    <ul>
-      {props.route.books.map((book, index) => <li key={index}>
-        {book.title} <Link to={`products/details/${index}`}>(details)</Link></li>)}
-    </ul>
-  </div>
-)
+
+class Product extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Our Products</h2>
+        <button onClick={this.onNewBook}>Add Book</button>
+        <h4>All our great books </h4>
+        <ul>
+          {this.props.route.books.map((book, index) => <li key={index}>
+            {book.title} <Link to={`products/details/${index}`}>(details)</Link></li>)}
+        </ul>
+      </div>
+    )
+  }
+
+  onNewBook = () =>{
+    hashHistory.push('products/newBook');
+  }
+}
+
+
 
 
 class RouterComponent extends React.Component {
- 
+
   render() {
     var books = this.props.bookStore.books;
     console.log(this.props.msg);
@@ -49,6 +66,7 @@ class RouterComponent extends React.Component {
               books={books} />
             <Route path="products/details/:id" component={Details}
               books={books} />
+            <Route path="products/newBook" component={NewBook} />
             <Route path="company" component={Company} />
             <Route path="blog" component={Blog} />
           </Route>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RouterComponent from './RouterComponent';
 import './index.css';
-import BookStore from "./BookStore";
-var bookStore = new BookStore();
+import bookStore from "./BookStore";
+//var bookStore = new BookStore();
 
 ReactDOM.render(
   <RouterComponent bookStore={bookStore} msg="Hello World"/>,
